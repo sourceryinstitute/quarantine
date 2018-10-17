fa4f0f7bbb47f50d9708473c393779132b5f7982
Damian Rouson <damian@sourceryinstitute.org>
2018-10-17 15:10:22 -0700

    Delete unused variables in main program hzetrn_trn

--- src/hzetrn_trn.f90
+++ src/hzetrn_trn.f90
@@ -1,6 +1,4 @@
-    Use total_mod, only : CLEN_SIZE, READ_TRNINPUT
+    Use total_mod, only : READ_TRNINPUT
     Implicit None
-    Integer                                                             :: Num_Mats_loc
-    Character(LEN=CLEN_SIZE), Allocatable, Dimension(:)                 :: Mat_Name_loc
     Call Read_TRNINPUT ('trninput.dat')
 End
